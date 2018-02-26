c1efef4734d61ad7f6d5401658c0103f0327d9f7
Kallu609 <khirvola@gmail.com>
2018-02-26 14:25:07 +0200

    vähä dokumentointii vähä

--- config.js
+++ config.js
@@ -5,10 +5,12 @@ const config = {
     password: '',
 
     // Lanka mihin postataan
-    thread: 'https://ylilauta.org/satunnainen/77386662',
+    thread: 'https://ylilauta.org/satunnainen/xxxxxxxx',
 
     // Kansio mistä dumpataan (__dirname = kansio missä app.js on)
     dir: path.join(__dirname, 'dumpthese'),
+    // Tai sit vaa näin
+    // dir: 'C:\\Joku\\Vitun\\sick\\kansio\\',
 
     // Sallitut tiedostotyypit
     types: ["mp4", "jpg", "png"],
